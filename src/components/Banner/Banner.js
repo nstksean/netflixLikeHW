a98816448cde79706ed0c06b54cdc83e76da926a
SeanLin <nstk.gm@gmail.com>
2022-11-20 15:44:36 +0800

    feat(component):make banner/Navbar/carousl
    NavBar already fit design in desktop version.

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,31 +1,22 @@
 import Carousel from 'react-bootstrap/Carousel'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-
 export default function Banner() {
     return (
         <Carousel>
-            <Carousel.Item interval={1000}>
+            <Carousel.Item interval={5000}>
                 <img
                     className="d-block w-100"
                     src="https://mdbootstrap.com/img/new/slides/041.jpg"
                     alt="First slide"
                 />
-                <Carousel.Caption>
-                    <h3>First slide label</h3>
-                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                </Carousel.Caption>
             </Carousel.Item>
-            <Carousel.Item interval={500}>
+            <Carousel.Item interval={5000}>
                 <img
                     className="d-block w-100"
                     src="https://mdbootstrap.com/img/new/slides/042.jpg"
                     alt="Second slide"
                 />
-                <Carousel.Caption>
-                    <h3>Second slide label</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                </Carousel.Caption>
             </Carousel.Item>
 
         </Carousel>
--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,17 +1,29 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import serachP from '../../asset/search.webp'
+import bigLogo from "../../asset/sawadika_500_250.png"
+import smallLogo from "../../asset/sawadika_300_80.png"
+import "./styles.css"
 
 function NavBar() {
+    const navWords = [`หนังฝรั่ง`, `หนังไทย`, `หนังเกาหลี`, `หนังญี่ปุ่น`, `หนังจีน`]
+
     return (
         <>
-            <Navbar bg="dark" variant="dark" sticky="top">
-                <Container>
-                    <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+            <Navbar className="navbar navbar-expand-lg navbar-dark fixed-top bg-dark opacity-75" >
+                <Container className='d-inline-flex'>
+                    <Navbar.Brand href="#home"><img className='bigLogo' src={bigLogo}></img></Navbar.Brand>
                     <Nav className="me-auto">
-                        <Nav.Link href="#home">Home</Nav.Link>
-                        <Nav.Link href="#features">Features</Nav.Link>
-                        <Nav.Link href="#pricing">Pricing</Nav.Link>
+                        {Array.from(navWords)?.map((navWord) => (
+                            <Nav.Link className='me-2' href='#'>{navWord}</Nav.Link>)
+                        )}
+                        <Navbar.Brand className='justify-content-star' href="#home"><img className='smallLogo' src={smallLogo}></img></Navbar.Brand>
+
+                    </Nav>
+                    <Nav className="justify-content-end">
+                        <Nav.Link><img src={`${serachP}`} alt='serach' className='serchP'></img></Nav.Link>
+                        <Nav.Link className='login'>สมัคร</Nav.Link>
                     </Nav>
                 </Container>
             </Navbar>
